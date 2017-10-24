3981ba601968b8dfc35fb34ee61e2e34c9afe477
Willian Kossmann da Silva <kossmannwillian@gmail.com>
2017-10-24 20:13:32 -0300

    testes com

---
+++ Integration Tests/state_machine(inicial)/testes/I2C/I2C.h
@@ -0,0 +1,41 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include "libi2c/Pi2c.h"
+#include <iostream>
+#include <string>
+#include <fstream>  
+#include <stdio.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+#define QTD_BYTES_I2C 10
+
+class I2C{
+public:
+    I2C();
+
+    char in[QTD_BYTES_I2C*4];   //Buf de entrada
+    char out[QTD_BYTES_I2C+1];  //Buf de saida
+
+    //Limpa o bufers de entrada e saida
+    void clearBufIn();
+    void clearBufOut();
+    
+    //Usado para receber dados apenas (é enviando uma msg padrão de receber dados) - 10ms
+    bool getData();
+
+    //Usado para enviar dados apenas (é recebido uma msg padrão de confirmação) - 10ms
+    bool sendData();
+
+    //Usado para enviar e receber dados ao mesmo tempo 
+    //( milisec - tempo de espera pode ser estipulado pelo usuário - max testado ???ms)
+    bool tradeData(int milisec = 20);
+
+    //Função para imprimir os dados recebidos, deve ser usado após um getData() ou tradeData()
+    void printData();
+
+};
+
+#endif
---
+++ Integration Tests/state_machine(inicial)/testes/I2C_Arduino/I2C_Arduino.ino
@@ -0,0 +1,111 @@
+#include <Wire.h>
+
+//Definicoes I2C:
+#define SLAVE_ADDRESS 0x04
+#define QTD_BYTES_I2C 10
+char in[QTD_BYTES_I2C*4];
+char out[QTD_BYTES_I2C] = {".........;"};
+char msgPadrao[QTD_BYTES_I2C] = {".........;"};
+void clearBuf(){
+  for (int i=0; i<QTD_BYTES_I2C; i++) {
+    in[i]='\0';
+    out[i]=msgPadrao[i];
+  }
+}
+//
+
+//Definicoes estados:
+int estadoAtual = 0, estadoAntigo = 0;
+//
+
+//Outras definicoes:
+// bool flag = false;
+bool erroCom = false;
+//
+
+//Delay sem ser delay:
+void delay2(int milsec){
+  unsigned int tempo;
+  while((millis() - tempo) < milsec){  }
+}
+//
+
+void setup() {
+  //I2C:
+  Wire.setTimeout(100);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  //
+
+  //Serial:
+  Serial.begin(9600);
+  //
+}
+
+void loop() {
+  // if(flag){
+  //   delay2(1000);
+  //   flag = false;
+  // }
+}
+
+void receiveData(int byteCount) {
+  if(byteCount != QTD_BYTES_I2C){
+    Serial.println("Erro!, Msg descartada!");
+    clearBuf();
+    while(Wire.available()) {
+      Wire.read();
+    }
+    clearBuf();
+    erroCom = true;
+  }else{
+    clearBuf();
+    while (Wire.available()) {
+      Wire.readBytesUntil(';', in, byteCount);
+    }
+    Serial.print("Recebido: ");
+    Serial.println(in); 
+    switch(in[0]){
+      case 1:
+        //Segue Parede:
+        estadoAntigo = estadoAtual;
+        estadoAtual = 1;
+
+      break;
+      case 2:
+        //Procura Copo:
+        estadoAntigo = estadoAtual;
+        estadoAtual = 2;
+
+      break;
+      case 3:
+        //Pega Copo:
+        estadoAntigo = estadoAtual;
+        estadoAtual = 3;
+
+      break;
+      case 'V':
+        out[0] = 'T';
+        out[1] = '0';
+        out[2] = '1';
+      break;
+      case 'D':
+        out[0] = 'T';
+        out[1] = '0';
+        out[2] = '2';
+      break;
+      default:
+          
+      break;
+    }
+  }
+}
+
+void sendData() {
+  if(erroCom){
+    Wire.write(msgPadrao, QTD_BYTES_I2C);
+  }else{
+    Wire.write(out, QTD_BYTES_I2C);
+  }
+}
