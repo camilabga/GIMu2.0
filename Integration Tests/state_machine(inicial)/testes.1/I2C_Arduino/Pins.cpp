b20c487aaa13c9b83db8a9a34fe82734cbfda2f4
Willian Kossmann da Silva <kossmannwillian@gmail.com>
2017-10-28 15:04:42 -0300

    teste i2c byte only

---
+++ Integration Tests/state_machine(inicial)/testes.1/I2C_Arduino/Pins.cpp
@@ -0,0 +1,28 @@
+// ### Pinagem Sharps Base 
+#define SH_DIREITA_TRAS 0 // [0]
+#define SH_DIREITA_FRENTE 1 // [1]
+#define SH_FRENTE_DIREITA 2 // [2]
+#define SH_FRENTE_ESQUERDA 3 // [3]
+#define SH_ESQUERDA_TRAS 4 // [4]
+#define SH_ESQUERDA_FRENTE 5 // [5]
+
+// ### Pinagem Motor DC Movimentação
+#define DC11 11 //Esquerdo
+#define DC12 10
+
+#define DC21 8 //Direito
+#define DC22 9
+
+// ### Pinagem Braco Garra
+#define SH_GARRA 7
+#define MSH_GARRA_D 8
+#define MSH_GARRA_E 9
+
+#define SERVOG_DEDO 2
+#define SERVOG_PULSO 3
+
+#define MBRACO1 4
+#define MBRACO2 5
+
+#define FDC_FRENTE 24 
+#define FDC_TRAS 25
---
+++ Integration Tests/state_machine(inicial)/testes.1/maquina_estados.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "I2C/I2C.h"
+
+using namespace std;
+
+int main(){
+	//Variaveis I2C:
+	I2C arduino;
+	//
+
+	//Variaveis Máquina_Estado:
+	int estadoAtual=1;
+	bool fim_geral = false;
+	//
+
+	while(!fim_geral){
+		switch(estadoAtual){
+			case 1:
+				//Segue Parede:
+				//Teste:
+				arduino.out[0] = 'V';
+				arduino.out[1] = 'D';
+				arduino.out[2] = 'C';
+				if(!arduino.tradeData()){
+					cout << "Erro ao enviar! " << endl;
+					return -1;
+				}
+				cout << "Enviado.. " << "Confirmacao: " << arduino.in << endl;
+				usleep(100000);
+				// if(!arduino.sendData()){
+				// 	cout << "Erro ao enviar! " << endl;
+				// 	return -1;
+				// }
+				// //
+				// if(!arduino.getData()){
+				// 	cout << "Erro ao receber! " << endl;
+				// 	return -1;
+				// }
+				arduino.out[0] = 'D';
+				arduino.out[1] = 'E';
+				arduino.out[2] = 'U';
+				if(!arduino.tradeData()){
+					cout << "Erro ao enviar! " << endl;
+					return -1;
+				}
+				//Teste
+				cout << "Enviado.. " << "Confirmacao: " << arduino.in << endl;
+				//TEste
+				usleep(100000);
+
+			break;
+			case 2:
+				//Procura Copo:
+			break;
+
+			default:
+				//Erro:
+			break;
+		}
+	}
+    return 0;
+}
